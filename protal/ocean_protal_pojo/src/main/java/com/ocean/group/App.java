b3859b20b550bc948277adee00d86c6709f9c360
jinhaiyang <jhy..1008611>
2019-03-28 14:56:09 +0800

    打包

--- protal/ocean_protal_pojo/src/main/java/com/ocean/group/App.java
+++ protal/ocean_protal_pojo/src/main/java/com/ocean/group/App.java
@@ -4,6 +4,7 @@ package com.ocean.group;
  * Hello world!
  *
  */
+
 public class App 
 {
     public static void main( String[] args )
--- protal/ocean_protal_web/src/main/java/com/ocean/basicinfo/basiccontroller/BasicInfoController.java
+++ protal/ocean_protal_web/src/main/java/com/ocean/basicinfo/basiccontroller/BasicInfoController.java
@@ -1,19 +1,18 @@
 package com.ocean.basicinfo.basiccontroller;
 
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 
 /**
  * @author 金海洋
  * @date 2019/3/27  -15:34
  */
-@Controller
-@RequestMapping("BasicInfoController")
+
+
 public class BasicInfoController {
 
-	private static final org.slf4j.Logger LOGGER= LoggerFactory.getLogger(BasicInfoController.class);
+
+
+
+
 
 
 
